018fa694a4afa21b1e17d93cc7c94bc8014f40f9
wxnet2013 <wxnet2013@gmail.com>
2018-04-04 16:13:48 +0800

    fix: 路由修复

--- webpack-demo/src/routes.js
+++ webpack-demo/src/routes.js
@@ -2,27 +2,34 @@ import React from 'react';
 import {
   HashRouter as Router,
   Switch,
-  Route
+  Route,
+  Link
 } from 'react-router-dom';
 
 const Home = () => (
-  <div>home</div>
+  <div>
+    home
+    <Link to="/about">关于</Link>
+  </div>
+);
+
+const About = () => (
+  <div>about</div>
 );
 
 const Login = () => (
   <div>Login</div>
 );
 
-const routes = () => {
-  return (
-    <Router>
-      <Switch>
-        <Route path="/" component={Home}/>
-        <Route path="/home" component={Home}/>
-        <Route path="/login" component={Login}/>
-      </Switch>
-    </Router>
-  );
-};
+const routes = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/" component={Home}/>
+      <Route path="/home" component={Home}/>
+      <Route path="/about" component={About}/>
+      <Route path="/login" component={Login}/>
+    </Switch>
+  </Router>
+);
 
 export default routes;
